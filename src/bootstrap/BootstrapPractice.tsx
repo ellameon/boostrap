cdac866ba9aafe3d445840a28af8e01e60ea7002
milyukov <milyukov@protei-lab.ru>
2022-11-28 08:04:08 +0300

    start commit

---
+++ src/bootstrap/BootstrapPractice.tsx
@@ -0,0 +1,15 @@
+import {observer} from "mobx-react-lite";
+
+export const BootstrapPractice = observer(function BootstrapPractice() {
+  return(
+    <div className="card">
+    <div className="card-header"></div>
+      <div className="card-body">
+
+      </div>
+      <div className="card-footer">
+
+      </div>
+    </div>
+  )
+})
